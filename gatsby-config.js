acb013b2dc6d3a38e1b43ea830497d4639207b5f
yeseul.park <yesleee@direa.co.kr>
2024-07-05 16:22:00 +0900

    feat: 배포를 위한 라이브러리 설치 및 세팅

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `parkksss's dev blog`,
+    description: `parkksss's blog for dev log`,
+    author: `parkksss`,
+    siteUrl: `https://parkksss.github.io/`,
   },
   plugins: [
     {
